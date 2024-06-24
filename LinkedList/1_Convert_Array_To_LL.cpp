02b067c5fa76e17053a43b64b1569f3e8b120a8d
Harsh Patil <harshpatil2956@gmail.com>
2024-06-24 11:20:08 +0530

    linkedlist

---
+++ LinkedList/1_Convert_Array_To_LL.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+// syntax of creating the linkedlist
+
+struct Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// converting the Array to the Linked List
